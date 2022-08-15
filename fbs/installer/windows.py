c5fc40b65ff4419654fa37041b13ee886165b966
Marcus Vogel <vogl91@gmail.com>
2022-08-15 13:10:43 +0200

    Print windows make installer output

--- fbs/installer/windows.py
+++ fbs/installer/windows.py
@@ -1,14 +1,11 @@
 from fbs import path
 from fbs.installer import _generate_installer_resources
-from subprocess import check_call, DEVNULL
+from subprocess import check_call
 
 def create_installer_windows():
     _generate_installer_resources()
     try:
-        check_call(
-            ['makensis', 'Installer.nsi'], cwd=path('target/installer'),
-            stdout=DEVNULL
-        )
+        check_call(['makensis', 'Installer.nsi'], cwd=path('target/installer'))
     except FileNotFoundError:
         raise FileNotFoundError(
             "fbs could not find executable 'makensis'. Please install NSIS and "
